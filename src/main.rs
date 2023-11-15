f7bd7b061642ea418d4ab8cd3d7405e7fbb7ba63
York <57304851+wlyh514@users.noreply.github.com>
2023-11-15 01:26:18 -0500

    feat: implement http1.1 pipelining

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,104 @@
-use std::{fs, net::{TcpListener, TcpStream}, io::{BufReader, prelude::*}};
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex, mpsc::{self, Receiver, Sender}},
+    thread::{self, JoinHandle},
+    time::Duration,
+    vec, collections::VecDeque,
+};
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+type ResponseQueue = VecDeque<JoinHandle<String>>;
 
-    // println!("Request: {:#?}", http_request);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+enum RespHandlerSignal {
+    NewResp, 
+    Finished,
+}
+
+fn handle_connection(stream: TcpStream) {
+    let response_queue: Arc<Mutex<ResponseQueue>> = Arc::new(Mutex::new(VecDeque::new()));
+    
+    let (resp_signal_tx, resp_signal_rx) = mpsc::channel::<RespHandlerSignal>();
+    let response_queue_cp = response_queue.clone();
+    let stream_cp = stream.try_clone().unwrap();
+    thread::spawn(move || handle_response(response_queue_cp, stream_cp, resp_signal_rx));
+
+    let buf_reader = BufReader::<TcpStream>::new(stream);
+
+    let mut http_request: Vec<String> = vec![];
+    let _body = false;
+    for line in buf_reader.lines() {
+        let line = line.unwrap();
+
+        if line.is_empty() {
+            // TODO: handle the case of request with body
+            
+            // Wrap up this request, send to request handler
+            let http_request_cp = http_request.clone();
+            let resp_signal_tx_cp = resp_signal_tx.clone();
+            let handler = thread::spawn(move || handle_request(http_request_cp, resp_signal_tx_cp));
+            {
+                let mut response_queue = response_queue.lock().unwrap();
+                response_queue.push_back(handler);
+                println!("New handler pushed");
+            }
+            http_request.clear();
+        } else {
+            http_request.push(line);
+        }
+    }
+    resp_signal_tx.send(RespHandlerSignal::Finished).unwrap();
+}
+
+fn handle_response(response_queue: Arc<Mutex<ResponseQueue>>, mut stream: TcpStream, resp_signal_rx: Receiver<RespHandlerSignal>) {
+    loop {
+        match resp_signal_rx.recv().unwrap() {
+            RespHandlerSignal::NewResp => {
+                let mut response_queue = response_queue.lock().unwrap();
+                while !response_queue.is_empty() {
+                    let handler = response_queue.pop_front().unwrap();
+                    thread::sleep(Duration::from_micros(1));
+                    if handler.is_finished() {
+                        let response = handler.join().unwrap();
+                        stream.write_all(response.as_bytes()).unwrap();
+                        println!("Response sent: {response}");
+                    } else {
+                        break;
+                    }
+                }
+            }, 
+            RespHandlerSignal::Finished => {
+                break;
+            }
+        }
+    }
+}
+
+fn handle_request(http_request: Vec<String>, resp_signal_tx: Sender<RespHandlerSignal>) -> String {
+    println!("Request: {:#?}", &http_request);
+    let request_line = http_request[0].clone();
     let mut splited_request_line = request_line.split_whitespace();
 
     let method = splited_request_line.next().unwrap();
-    let uri = splited_request_line.next().unwrap(); 
-    let protocol = splited_request_line.next().unwrap();
+    let uri = splited_request_line.next().unwrap();
+    let _protocol = splited_request_line.next().unwrap();
 
     let (status_line, file_name) = match (method, uri) {
-        ("GET", "/") => ("HTTP/1.1 200 OK", "index.html"), 
+        ("GET", "/") => ("HTTP/1.1 200 OK", "index.html"),
+        (_, "/slow") => {
+            // Simulate long processing time
+            thread::sleep(Duration::from_secs(10));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(format!("static/{file_name}")).unwrap();
     let content_length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}\r\n");
 
-    let response = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
+    resp_signal_tx.send(RespHandlerSignal::NewResp).unwrap();
+    return response;
 }
 
 fn main() {
---
+++ tests/test.rs
@@ -0,0 +1,70 @@
+#[cfg(test)]
+mod tests {
+    use std::{
+        io::{prelude::*, BufReader, Write},
+        net::TcpStream,
+        sync::mpsc::{self, Sender},
+        thread,
+        time::{Duration, SystemTime, UNIX_EPOCH},
+    };
+
+    fn send_slow_request(mut stream: TcpStream) {
+        stream
+            .write_all(
+                "GET /slow HTTP/1.1\r\nHost: localhost:7878\r\nConnection: keep-alive\r\n\r\n"
+                    .as_bytes(),
+            )
+            .unwrap();
+        println!("GET /slow request sent.");
+    }
+
+    enum RespListenerSignal {
+        NewResp,
+        Stop,
+    }
+    fn listen_to_response(stream: TcpStream, signal_tx: Sender<RespListenerSignal>) {
+        let buf_reader = BufReader::new(stream);
+        for line in buf_reader.lines().map(|result| result.unwrap()) {
+            if line.starts_with("HTTP/1.1") {
+                println!("Response Received");
+                signal_tx.send(RespListenerSignal::NewResp).unwrap();
+            }
+            println!("{line}");
+        }
+        println!("Connection closed");
+        signal_tx.send(RespListenerSignal::Stop).unwrap();
+    }
+
+    #[test]
+    fn test_pipelining() {
+        let stream = TcpStream::connect("localhost:7878").unwrap();
+        let (signal_tx, signal_rx) = mpsc::channel::<RespListenerSignal>();
+
+        let stream_cp = stream.try_clone().unwrap();
+        let resp_listener_handler = thread::spawn(move || listen_to_response(stream_cp, signal_tx));
+
+        const NUM_REQUESTS: u32 = 3;
+
+        for _ in 0..NUM_REQUESTS {
+            send_slow_request(stream.try_clone().unwrap());
+        }
+
+        let mut prev_response_at = SystemTime::from(UNIX_EPOCH);
+        for i in 0..NUM_REQUESTS {
+            match signal_rx.recv().unwrap() {
+                RespListenerSignal::NewResp => {
+                    let now = SystemTime::now();
+                    if i > 0 {
+                        assert!(
+                            now.duration_since(prev_response_at).unwrap() < Duration::from_secs(10)
+                        );
+                    }
+                    prev_response_at = SystemTime::now();
+                }
+                RespListenerSignal::Stop => assert!(false),
+            }
+        }
+        drop(stream);
+        // TODO: Test whether the responses are returned in the sending order of requests.
+    }
+}
